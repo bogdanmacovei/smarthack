5daa4c4987a9c150863a3ab372466c3d4052f3e6
lucianzr1 <lucianboaca97@gmail.com>
2017-10-21 19:53:20 +0300

    Removed a space

--- smarthack_node/app.js
+++ smarthack_node/app.js
@@ -5,7 +5,7 @@ var bodyParser = require ('body-parser');
 var express = require ('express');
 var bcrypt = require('bcryptjs');
 
-var app = express ();
+var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
